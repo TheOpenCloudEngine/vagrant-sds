38cffb848045c30e326738178c04d2ee1ac08a94
Sang Song <swsong_dev@websqrd.com>
2014-12-23 07:54:35 +0900

    node와 collector를 나누어 빌드하도록.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,18 @@
+#!/bin/sh
+# This shell provisions node container and log collector container
+# Jin Won Cho, Sang Song
+# 2014-12-22
+#
+
 sudo apt-get install -y git
+
 git clone https://github.com/uengine/build-sds.git
-cd build-sds
+CURPATH=`pwd`
+
+# Install Wildfly server and Fluentd client agent
+cd $CURPATH/build-sds/node
+sudo sh -x build.sh
+
+# Install Fluentd server (log collector)
+cd $CURPATH/build-sds/collector
 sudo sh -x build.sh
